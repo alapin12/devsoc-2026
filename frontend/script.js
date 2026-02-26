2c384bf524291489a11b98e1b8c950bcda361294
Charlie Wei <charliewei365@gmail.com>
2026-02-27 07:36:22 +1100

    make image gallery display json data

--- frontend/script.js
+++ frontend/script.js
@@ -1,2 +1,21 @@
-fetch("data.json");
+const galleryEl = document.querySelector('.gallery');
 
+fetch('./data.json')
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(element => {
+            const cardEl = document.createElement('div');
+            cardEl.classList.add('card');
+            cardEl.innerHTML = `            
+                <img src="./assets/${element.building_picture}">
+                <div class="status-tag">
+                <span class="dot"></span>
+                ${element.rooms_available} rooms available 
+                </div>
+                <div class="room-tag">
+                    ${element.name}
+                </div>
+            `;
+            galleryEl.appendChild(cardEl);
+        });
+    })
